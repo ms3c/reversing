c765ee13a9e860691905abb3f206fe6b919674ab
sn0wd3n <63225928+ms3c@users.noreply.github.com>
2020-06-28 00:50:48 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -97,7 +97,7 @@ string Base64Encode(string vInput) {
 
  int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
-    MessageBox ( NULL, "Hello World!", "Test", MB_OK );
+    
     return 0;
 }
 
